caa3e13c56ef3a81221efb05fb0ee5792cf28a10
harnalerohan <harnalerohan@gmail.com>
2022-12-27 18:50:44 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroSectionComponent } from './shared/components/hero-section/hero-section.component';
+import { FeaturesComponent } from './shared/components/features/features.component';
+import { ContactComponent } from './shared/components/contact/contact.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroSectionComponent,
+    FeaturesComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
